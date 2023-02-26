265152649d19f68fe64914618d2a27cf1bcf2e72
Josu Alonso <josu93alonso@gmail.com>
2023-02-26 16:24:42 +0100

    dividing endpoint into HTML and JSON

--- src/app.py
+++ src/app.py
@@ -3,20 +3,32 @@ from detector import DetectorPipeline
 
 app = Flask(__name__)
 
+def inference(text):
+    pipeline = DetectorPipeline()
+    tokenizer, model = pipeline.get_tokenizer_and_model("JosuMSC/fake-news-detector")
+    prediction, logits = pipeline.predict(tokenizer, model, text)
+    logits = logits.cpu().detach().numpy().tolist()
+    result = 'Fake' if prediction == 0 else 'Real'
+    return result, logits
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/detect', methods=['POST'])
-def detect():
+@app.route('/detect_html', methods=['POST'])
+def detect_html():
     if request.method == 'POST':
         text = request.form['text']
-        pipeline = DetectorPipeline()
-        tokenizer, model = pipeline.get_tokenizer_and_model("JosuMSC/fake-news-detector")
-        prediction, logits = pipeline.predict(tokenizer, model, text)
-        inference = 'Fake' if prediction == 0 else 'Real'
+        result, logits = inference(text)
+        return render_template('index.html', result=result, logits=logits)
 
-        return jsonify(inference=inference)
+@app.route('/detect_json', methods=['POST'])
+def detect_json():
+    if request.method == 'POST':
+        text = request.json['text']
+        result, logits = inference(text)
+        return jsonify(result=result, logits=logits)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
