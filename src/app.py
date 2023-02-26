aab77cb7381c2dfac0f405c4b02e957c15a49383
Josu Alonso <josu93alonso@gmail.com>
2023-02-26 14:40:24 +0100

    Flask API to use model

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+from detector import DetectorPipeline
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    if request.method == 'POST':
+        text = request.form['text']
+        pipeline = DetectorPipeline()
+        tokenizer, model = pipeline.get_tokenizer_and_model("JosuMSC/fake-news-detector")
+        prediction, logits = pipeline.predict(tokenizer, model, text)
+        inference = 'Fake' if prediction == 0 else 'Real'
+
+        return jsonify(inference=inference)
+
+if __name__ == '__main__':
+    app.run(debug=True)
