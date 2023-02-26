1f3bf0d5071347f606c0b1abc1749ccde7c038e8
Josu Alonso <josu93alonso@gmail.com>
2023-02-26 16:29:49 +0100

    Eliminate debug mode

--- src/app.py
+++ src/app.py
@@ -31,4 +31,4 @@ def detect_json():
         return jsonify(result=result, logits=logits)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
